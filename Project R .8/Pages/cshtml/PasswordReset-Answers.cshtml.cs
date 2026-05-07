5d71871a2b3a87a677a5fc6206f41aac8038adbd
hamorkid <attiasiaty@gmail.com>
2026-05-07 19:23:19 +0300

    make password recovery

--- Project R .8/Pages/cshtml/PasswordReset-Answers.cshtml.cs
+++ Project R .8/Pages/cshtml/PasswordReset-Answers.cshtml.cs
@@ -5,8 +5,44 @@ namespace Project_R_._8.Pages.cshtml
 {
     public class PasswordReset_AnswersModel : PageModel
     {
+        public string ErrorMessage { get; set; } = "";
+        public string Q1 { get; set; } = "";
+        public string Q2 { get; set; } = "";
+        [BindProperty] public string A1 { get; set; } = "";
+        [BindProperty] public string A2 { get; set; } = "";
+        public string ErrorMessage { get; set; } = "";
+
+        private readonly DBHelper _db;
+        private readonly PasswordReset_UsernameModel UsernameModel;
+
+
+        public PasswordReset_AnswersModel(DBHelper db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            DataTable user = _db.GetData(
+                $"SELECT * FROM Users WHERE UserName = '{UsernameModel.SendUsername()}'"
+            );
+            
+            Q1 = user.Rows[0]["Q1"].ToString()!;
+            Q2 = user.Rows[0]["Q2"].ToString()!;
+        }
+
+        public IActionResult OnPost()
+        {
+            DataTable user = _db.GetData(
+                $"SELECT * FROM Users WHERE UserName = '{UsernameModel.SendUsername()}'"
+            );
+
+            if (!((A1 == user.Rows[0]["A1"].ToString()!) && (A2 == user.Rows[0]["A2"].ToString()!)))
+            {
+                ErrorMessage = "Please recheck your answers";
+                return Page();
+            }
+            return RedirectToPage("/cshtml/PasswordReset-Answers"); 
         }
     }
 }
--- Project R .8/Pages/cshtml/PasswordReset-Username.cshtml.cs
+++ Project R .8/Pages/cshtml/PasswordReset-Username.cshtml.cs
@@ -5,8 +5,37 @@ namespace Project_R_._8.Pages.cshtml
 {
     public class PasswordReset_UsernameModel : PageModel
     {
+        public string ErrorMessage { get; set; } = "";
+        [BindProperty] public string UserName { get; set; } = "";
+        private readonly DBHelper _db;
+
+        public PasswordReset_AnswersModel(DBHelper db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+
+        }
+        public IActionResult OnPost()
+        {    
+            DataTable user = _db.GetData(
+                $"SELECT * FROM Users WHERE UserName = '{Username}'"
+            );
+
+            if (user.Rows.Count == 0)
+            {
+                ErrorMessage = "Invalid username";
+                return Page();
+            }
+
+            SendUsername();
+            return RedirectToPage("/cshtml/PasswordReset-Answers")
+        }
+        public string SendUsername()
+        {
+            return UserName;
         }
     }
 }
